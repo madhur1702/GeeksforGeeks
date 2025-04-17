da92c27db1b881a04e6bf086fa8d046927dec804
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2025-04-17 20:21:29 +0530

    Create Second_Largest.cpp

---
+++ Second_Largest.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+  public:
+    int getSecondLargest(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        sort(arr.begin(),arr.end());
+        for(int i=n-1;i>0;i--)
+        {
+            if(arr[i]!=arr[i-1])
+            {
+                return arr[i-1];
+            }
+        }
+        return -1;
+    }
+};
