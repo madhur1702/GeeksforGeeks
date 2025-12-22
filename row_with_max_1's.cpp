2d8e267a15cf3c523f04e6b95e27b10bbb426c13
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2025-12-22 11:11:44 +0530

    Create row_with_max_1's.cpp

---
+++ row_with_max_1's.cpp
@@ -0,0 +1,25 @@
+// User function template for C++
+class Solution {
+  public:
+    int rowWithMax1s(vector<vector<int>> &arr) {
+        // code here
+        int maxrow = 0,maxi = 0;
+        for(int i = 0;i<arr.size();i++)
+        {
+            int total = 0;
+            for(int j = 0;j<arr[i].size();j++)
+            {
+                if(arr[i][j]==1)
+                {
+                    total += 1;
+                }
+            }
+            if(maxi < total)
+            {
+                maxi = total;
+                maxrow = i;
+            }
+        }
+        return maxrow;
+    }
+};
