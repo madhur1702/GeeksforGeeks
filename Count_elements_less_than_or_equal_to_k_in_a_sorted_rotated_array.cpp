0d601b10473d17a2025846835dc2dbc2f2e519c4
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2025-12-24 12:32:21 +0530

    Create Count_elements_less_than_or_equal_to_k_in_a_sorted_rotated_array.cpp

---
+++ Count_elements_less_than_or_equal_to_k_in_a_sorted_rotated_array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public:
+    int countLessEqual(vector<int>& arr, int x) {
+        // code here
+        int count = 0;
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i]<=x)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
