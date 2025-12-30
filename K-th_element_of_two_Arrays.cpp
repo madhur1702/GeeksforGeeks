d455e270ed45c25d649a3cdd5d87e22d37ab9183
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2025-12-30 18:09:57 +0530

    Update K-th_element_of_two_Arrays.cpp

--- K-th_element_of_two_Arrays.cpp
+++ K-th_element_of_two_Arrays.cpp
@@ -1,4 +1,4 @@
-
+/*
 class Solution {
   public:
     int kthElement(vector<int>& a, vector<int>& b, int k) {
@@ -14,5 +14,39 @@ class Solution {
         }
         sort(c.begin(),c.end());
         return c[k-1];
+    }
+};
+*/
+class Solution {
+  public:
+    int kthElement(vector<int> &a, vector<int> &b, int k) {
+        // code here
+        vector<int> arr;
+        int n = a.size();
+        int m = b.size();
+        int i =0,j = 0;
+        while(i<n && j<m)
+        {
+            if(a[i]<=b[j])
+            {
+                arr.push_back(a[i]);
+                i++;
+            }else
+            {
+                arr.push_back(b[j]);
+                j++;
+            }
+        }
+        while(i<n)
+        {
+            arr.push_back(a[i]);
+            i++;
+        }
+        while(j<m)
+        {
+            arr.push_back(b[j]);
+            j++;
+        }
+        return arr[k-1];
     }
 };
