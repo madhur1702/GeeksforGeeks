ac6e71464373f9206d5c265c36ff6fab384c8f23
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2026-01-02 20:24:30 +0530

    Add sorting function for 0s, 1s, and 2s
    Implemented Dutch National Flag algorithm to sort an array containing 0s, 1s, and 2s.

---
+++ Sort_0s,_1s_and_2s.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        // code here
+        int f = 0,m = 0,l = arr.size()-1;
+        while(m<=l)
+        {
+            if(arr[m]==1)
+            {
+                m++;
+            }else if(arr[m] == 0)
+            {
+                swap(arr[f],arr[m]);
+                m++;
+                f++;
+            }else
+            {
+                swap(arr[l],arr[m]);
+                l--;
+            }
+        }
+        
+    }
+};
