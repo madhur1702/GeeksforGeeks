efe3965fb32aa38502cbf6ebda4c4165c171a6aa
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2026-02-19 11:40:07 +0530

    Create Missing_Element_in_Range.cpp

---
+++ Missing_Element_in_Range.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    vector<int> missinRange(vector<int>& arr, int low, int high) {
+        // code here
+        unordered_set<int> st(arr.begin(),arr.end());
+        vector<int> nums;
+        for(int i = low;i<=high;i++)
+        {
+            if(st.find(i) == st.end())
+            {
+                nums.push_back(i);
+            }
+        }
+        return nums;
+    }
+};
