306b870cf3953590f19e6247884e81006d48f32e
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2026-02-21 20:21:03 +0530

    Fix formatting of variable initialization in hIndex

--- Find_H-index.cpp
+++ Find_H-index.cpp
@@ -3,7 +3,7 @@ class Solution {
     int hIndex(vector<int>& citations) {
         // code here
         sort(citations.begin(),citations.end(),greater<int>());
-        int h = 0;
+        int h = 0;  
         for(int i = 0;i<citations.size();i++)
         {
             if(citations[i]>=i+1)
