0df39e6d9cae58afb89a582aff22a5e87dd20d04
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2026-03-02 19:33:56 +0530

    Create Trapping_Rain_Water.cpp

---
+++ Trapping_Rain_Water.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int maxWater(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        vector<int> left(n),right(n);
+        left[0] = arr[0];
+        for(int i=1;i<n;i++)
+        {
+            left[i] = max(left[i-1],arr[i]);
+        }
+        right[n-1] = arr[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            right[i] = max(right[i+1],arr[i]);
+        }
+        int water = 0;
+        for(int i=0;i<n;i++)
+        {
+            water += min(left[i],right[i]) - arr[i];
+        }
+        return water;
+    }
+};
